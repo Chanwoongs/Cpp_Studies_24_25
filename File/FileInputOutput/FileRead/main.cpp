d1ec6b2314ce211a5801793a4295d16fde95857f
Chanwoong Bang <tkfkdcksdnd@daum.net>
2024-12-30 16:15:54 +0900

    C 파일 입출력

---
+++ File/FileInputOutput/FileRead/main.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+int main()
+{
+	char buffer[256];
+	FILE* file = nullptr;
+
+	fopen_s(&file, "Test.txt", "rt");
+
+	if (file != nullptr)
+	{
+		// 버퍼에 내용 읽어들이기
+		while (true)
+		{
+			//if (fgets(buffer, 256, file) == nullptr)
+			if (fgets(buffer, 10, file) == nullptr)
+			{
+				break;
+			}
+			std::cout << buffer;
+		}
+
+		fclose(file);
+	}
+
+	FILE* file2 = nullptr;
+
+	fopen_s(&file2, "Test.txt", "rt");
+
+	// feof 사용 좀 더 일반적
+	if (file2 != nullptr)
+	{
+		// 버퍼에 내용 읽어들이기
+		while (!feof(file2))
+		{
+			fgets(buffer, 10, file2);
+
+			std::cout << buffer;
+		}
+
+		fclose(file2);
+	}
+}
